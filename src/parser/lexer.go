6292d0a887d462d8b6770e49f89f665d7d8f913b
acroisie <arnaud.croisier@live.fr>
2025-01-11 17:05:07 +0100

    Good lexer.go, miammm

--- src/parser/lexer.go
+++ src/parser/lexer.go
@@ -3,24 +3,22 @@ package parser
 import (
 	"strings"
 	"unicode"
-
-	"golang.org/x/tools/go/callgraph/cha"
 )
 
 type TokenType int
 
 const (
-    TKN_EOF TokenType = iota
-    TKN_VAR        // A..Z
-    TKN_NOT        // '!'
-    TKN_AND        // '+'
-    TKN_OR         // '|'
-    TKN_XOR        // '^'
-    TKN_LPAREN     // '('
-    TKN_RPAREN     // ')'
-    TKN_IMPLIES    // '=>'
-    TKN_IFF        // '<=>'
-    TKN_UNKNOWN
+	TKN_EOF     TokenType = iota
+	TKN_VAR               // A..Z
+	TKN_NOT               // '!'
+	TKN_AND               // '+'
+	TKN_OR                // '|'
+	TKN_XOR               // '^'
+	TKN_LPAREN            // '('
+	TKN_RPAREN            // ')'
+	TKN_IMPLIES           // '=>'
+	TKN_IFF               // '<=>'
+	TKN_UNKNOWN
 )
 
 type Token struct {
@@ -59,7 +57,7 @@ func (l *Lexer) peekChar() rune {
 }
 
 func (l *Lexer) NextToken() Token {
-	for unicode.IsSpace(l.peekChar()){
+	for unicode.IsSpace(l.peekChar()) {
 		l.nextChar()
 	}
 
@@ -69,6 +67,38 @@ func (l *Lexer) NextToken() Token {
 	case 0:
 		return Token{Type: TKN_EOF, Value: ""}
 	case '+':
-		return Token{Type: TKN_AND, Value: '+'}
+		return Token{Type: TKN_AND, Value: "+"}
+	case '|':
+		return Token{Type: TKN_OR, Value: "|"}
+	case '^':
+		return Token{Type: TKN_XOR, Value: "^"}
+	case '!':
+		return Token{Type: TKN_NOT, Value: "!"}
+	case '(':
+		return Token{Type: TKN_LPAREN, Value: "("}
+	case ')':
+		return Token{Type: TKN_RPAREN, Value: ")"}
+	case '<':
+		if l.peekChar() == '=' {
+			l.nextChar()
+			if l.peekChar() == '>' {
+				l.nextChar()
+				return Token{Type: TKN_IFF, Value: "<=>"}
+			}
+			return Token{Type: TKN_UNKNOWN, Value: "<"}
+		}
+		return Token{Type: TKN_UNKNOWN, Value: "<"}
+	case '=':
+		if l.peekChar() == '>' {
+			l.nextChar()
+			return Token{Type: TKN_IMPLIES, Value: "=>"}
+		}
+		return Token{Type: TKN_UNKNOWN, Value: "="}
 
+	default:
+		if unicode.IsLetter(char) {
+			return Token{Type: TKN_VAR, Value: string(char)}
+		}
+		return Token{Type: TKN_UNKNOWN, Value: string(char)}
+	}
 }
