c52bc58e377fe19abd5a99bb67a2342d812663be
acroisie <arnaud.croisier@live.fr>
2025-01-20 21:55:07 +0100

    WIP on parser

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -1,8 +1,8 @@
 package parser
 
 import (
-	"fmt"
 	"expert-system/src/rules"
+	"fmt"
 )
 
 type Parser struct {
@@ -27,8 +27,8 @@ func (p *Parser) ParseRule() (*rules.Rule, error) {
 		return nil, err
 	}
 
-	if p.currTok.Type != TKN_IMPLIES || p.currTok.Type != TKN_IFF {
-		return nil, fmt.Errorf("Expected '=>'or '<=>' but got %s", p.currTok)
+	if p.currTok.Type != TKN_IMPLIES && p.currTok.Type != TKN_IFF {
+		return nil, fmt.Errorf("expected '=>'or '<=>' but got %v", p.currTok)
 	}
 
 	var op rules.ConditionalOperator
@@ -46,12 +46,118 @@ func (p *Parser) ParseRule() (*rules.Rule, error) {
 	}
 
 	r := &rules.Rule{
-		Op: op,
-		LeftExpressionGroup: leftExpr,
+		Op:                   op,
+		LeftExpressionGroup:  leftExpr,
 		RightExpressionGroup: rightExpr,
 	}
 
 	return r, nil
 }
 
-func (p *Parser) parseExpression() (*rules.ExpressionGroup, error) {return nil, nil} // TODO
+func (p *Parser) parseExpression() (*rules.ExpressionGroup, error) {
+	left, err := p.parseTerm()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.currTok.Type == TKN_OR || p.currTok.Type == TKN_XOR {
+		opTok := p.currTok
+		p.nextToken()
+
+		right, err := p.parseTerm()
+		if err != nil {
+			return nil, err
+		}
+
+		node := &rules.ExpressionGroup{}
+		if opTok.Type == TKN_OR {
+			node.Op = rules.OR
+		} else {
+			node.Op = rules.XOR
+		}
+
+		node.LeftExpressionGroup = left
+		node.RightExpressionGroup = right
+
+		left = node
+	}
+
+	return left, nil
+}
+
+func (p *Parser) parseTerm() (*rules.ExpressionGroup, error) {
+	left, err := p.parseFactor()
+	if err != nil {
+		return nil, err
+	}
+
+	for p.currTok.Type == TKN_AND {
+		p.nextToken()
+
+		right, err := p.parseFactor()
+		if err != nil {
+			return nil, err
+		}
+		node := &rules.ExpressionGroup{
+			Op:                   rules.AND,
+			LeftExpressionGroup:  left,
+			RightExpressionGroup: right,
+		}
+		left = node
+	}
+
+	return left, nil
+}
+
+func (p *Parser) parseFactor() (*rules.ExpressionGroup, error) {
+	if p.currTok.Type == TKN_NOT {
+		p.nextToken()
+
+		expr, err := p.parseFactor()
+		if err != nil {
+			return nil, err
+		}
+
+		if expr.Op == rules.NOTHING && expr.LeftVariable != nil && expr.RightVariable == nil {
+			expr.LeftVariable.Not = !expr.LeftVariable.Not
+			return expr, nil
+		}
+
+		newNode := &rules.ExpressionGroup{
+			Op:                  rules.NOTHING,
+			LeftVariable:        &rules.Variable{Letter: 0, Not: true},
+			LeftExpressionGroup: expr,
+		}
+
+		return newNode, nil
+	}
+
+	if p.currTok.Type == TKN_LPAREN {
+		p.nextToken()
+
+		expr, err := p.parseExpression()
+		if err != nil {
+			return nil, err
+		}
+		if p.currTok.Type != TKN_RPAREN {
+			return nil, fmt.Errorf("expected ')' but got %v", p.currTok)
+		}
+		p.nextToken()
+
+		return expr, nil
+	}
+
+	if p.currTok.Type == TKN_VAR {
+		node := &rules.ExpressionGroup{
+			Op: rules.NOTHING,
+			LeftVariable: &rules.Variable{
+				Letter: rune(p.currTok.Value[0]),
+				Not:    false,
+			},
+		}
+		p.nextToken()
+		return node, nil
+	}
+
+	return nil, fmt.Errorf("expected variable, '(', '!' but got %v", p.currTok)
+}
